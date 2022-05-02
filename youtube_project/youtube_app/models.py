d29439f381075cc0cfadc93a441e13b6606b518a
Lulzpower <Zaddicus1@gmail.com>
2022-05-02 12:19:35 -0600

    updating unsaved changes for previous commit

--- youtube_project/youtube_app/models.py
+++ youtube_project/youtube_app/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Comment
 
 # Create your models here.
 
 class Comment(models.model):
-  user = models.ForeignKey
+  user = models.ForeignKey(Comment, on_delete=models.CASCADE)
   video_id = models.CharField(max_length=255)
   text = models.CharField(max_length=255)
   likes = models.IntegerField()
   dislikes = models.IntegerField()
 
 class Reply(models.model):
-  user = models.ForeignKey
-  comment = models.ForeignKey
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
   text = models.CharField(max_length=255)
--- youtube_project/youtube_app/views.py
+++ youtube_project/youtube_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import filters
+from .models import Comment
+from .models import Reply
+from .serializers import CommentSerializer
+from .serializers import ReplySerializers
 
 # Create your views here.
+
+
