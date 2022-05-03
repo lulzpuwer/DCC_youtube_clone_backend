cb7c57e49c5bd8435ce45b336cb91cb8573b4997
Lulzpower <Zaddicus1@gmail.com>
2022-05-03 11:02:04 -0600

    created authentication app, created all views, serializers, models for the authentication app + fixed import error with djangorestframework-simplejwt

---
+++ youtube_project/authentication/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+    '''
+    This is a custom version of the built in User class
+    It contains all of the built in fields and functionality of the standard User
+    You can add fields here for any additional properties you want a User to have
+    This is useful for adding roles (Customer and Employee, for example)
+    For just a few roles, adding boolean fields is advised
+    '''
