19a438bbb6ff70584ba76dff8ea5f3c36527f229
Alex <thatsoalex@gmail.com>
2022-05-20 12:10:47 -0500

    Added mysql-connector-python + Modified Serializers

--- youtube_project/youtube_app/serializers.py
+++ youtube_project/youtube_app/serializers.py
@@ -4,11 +4,12 @@ from .models import Comment, Reply
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['user', 'video_id', 'text', 'likes', 'dislikes']
-        depth = 1
+        fields = ['id', 'user', 'video_id', 'text', 'likes', 'dislikes']
+       
 
 class ReplySerializer(serializers.ModelSerializer):
   class Meta:
     model = Reply
-    fields = ['user', 'comment', 'text']
+    fields = ['id','user', 'comment', 'text','user_id', 'commentt_id']
     depth = 1
+  comment_id = serializers.IntegerField(write_only = True)
