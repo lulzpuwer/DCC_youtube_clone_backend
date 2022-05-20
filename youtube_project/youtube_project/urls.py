f8a93b6eb7bad84ea1965c25292df6dd8dc9e70f
Alex <thatsoalex@gmail.com>
2022-05-20 11:57:41 -0500

    Modified urls

--- youtube_project/youtube_project/urls.py
+++ youtube_project/youtube_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
-    path['api/', include(('youtube_app.urls'))]
+    path('api/', include(('youtube_app.urls')))
 ]
